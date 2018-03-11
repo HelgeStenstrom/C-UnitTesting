6bc00289873e9a01182ad4037effad28543bd2db
Helge Stenström <h.stenstrom@gmail.com>
2018-03-11 21:31:57 +0100

    Osparad klass. Vad är den till för?

---
+++ UnitTestingz/Class1.cs
@@ -0,0 +1,25 @@
+﻿namespace Demo
+{
+    public class Class1
+    {
+        private class doublel
+        {
+        }
+
+        void Calculate(int number1, double number2, doublel number3)
+        {
+            //code 
+        }
+
+        void Calculate(int number1, double number2)
+        {
+            //code
+        }
+
+        double Calculate(double number1, double number2, doublel number3)
+        {
+            return 3.14;
+            //code 
+        }
+    }
+}
